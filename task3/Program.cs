c01b0f7e7e0ce9df644b5365e5b5ad275f504d8d
Ulugbek Khodzhayev <ukhodzhayev@inbox.ru>
2023-12-28 21:33:21 +0300

    comment task3, ass task4:LastHW

--- task3/Program.cs
+++ task3/Program.cs
@@ -1,11 +1,11 @@
-﻿int n = 97;
-int res1 = n / 10;
-int res2 = n % 10;
-if (res1 > res2)
-{
-    Console.WriteLine(res1);
-}
-else
-{
-    Console.WriteLine(res2);
-}
+﻿//int n = 97;
+//int res1 = n / 10;
+//int res2 = n % 10;
+//if (res1 > res2)
+//{
+  //  Console.WriteLine(res1);
+//}
+//else
+//{
+  //  Console.WriteLine(res2);
+//}
---
+++ task4/Program.cs
@@ -0,0 +1,9 @@
+﻿int n = 8961569;
+int i = 1;
+int num = n % 10;
+while (i <= n)
+{
+    num = n % 10;
+    Console.Write($"{num}, ");
+    n = n / 10;
+}
