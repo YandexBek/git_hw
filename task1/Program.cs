74c47fd25415cbea23e3b4fd11e12e0a27871b7f
Ulugbek Khodzhayev <ukhodzhayev@inbox.ru>
2023-12-28 19:54:55 +0300

    add task1:LastHW

---
+++ task1/Program.cs
@@ -0,0 +1,19 @@
+﻿int a = 161;
+int b = a % 7;
+int c = a % 23;
+if (b == 0)
+{
+    Console.WriteLine($"Число `{a}` кратно 7");
+}
+else
+{
+    Console.WriteLine($"Число `{a}`не кратно 7");
+}
+if (c == 0)
+{
+    Console.WriteLine($"Число `{a}` кратно 23");
+}
+else
+{
+    Console.WriteLine($"Число `{a}`не кратно 23");
+}
